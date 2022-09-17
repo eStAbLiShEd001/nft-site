7a44f2b79cf1da886ba86cb9dd721f15fe31bef7
established001 <blaccvibes@gmail.com>
2022-09-17 16:56:36 -0700

    First commit, Still working on mobile version.

---
+++ live.js
@@ -0,0 +1,7 @@
+// const dropbtn = document.querySelector('.dropbtn');
+// const content = document.querySelector('.dropdown-content')
+// const show = document.querySelector('.show')
+// dropbtn.addEventListener('click', function(){
+//     show.classList.remove('dropdown-content')
+// })
+
